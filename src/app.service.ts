6f0cc1d62deed045df81092e3880ab36f9853324
tansincosy <805841483@qq.com>
2022-03-12 23:03:43 +0800

    feat: 新增winston 日志

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 @Injectable()
 export class AppService {
+  constructor(@Inject(Logger) private readonly logger: LoggerService) {}
   getHello(): string {
+    // this.logger.info('sss %s', 'ssss');
+    this.logger.debug('ccc');
+    this.logger.error('sssss');
+    this.logger.warn('cccc');
     return 'Hello World!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('donkey', {
+              prettyPrint: true,
+            }),
+          ),
+          level: 'debug',
+        }),
+      ],
+    }),
+  });
   await app.listen(3000);
 }
 bootstrap();
